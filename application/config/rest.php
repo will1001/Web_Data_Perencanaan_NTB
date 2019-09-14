36f4a3b564aa01006d42b03db4c26a6278c661c7
FiqriJml <jamalfiqri@gmail.com>
2019-09-14 13:58:28 +0800

    Membuat otentikasi Api Key dan membuat tabel keys

--- application/config/rest.php
+++ application/config/rest.php
@@ -324,7 +324,8 @@ $config['rest_keys_table'] = 'keys';
 |   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
 */
-$config['rest_enable_keys'] = FALSE;
+//ganti jadi true
+$config['rest_enable_keys'] = true;
 
 /*
 |--------------------------------------------------------------------------
@@ -377,7 +378,8 @@ $config['rest_key_length'] = 40;
 | 2012/06/12. See RFC 6648 specification for more details
 |
 */
-$config['rest_key_name'] = 'X-API-KEY';
+//ganti api-key
+$config['rest_key_name'] = 'WDP-NTB-KEY';
 
 /*
 |--------------------------------------------------------------------------
