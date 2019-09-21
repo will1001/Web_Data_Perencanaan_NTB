645c8d3f3a6fc1adc2d050dbff69e36e25e4226f
FiqriJml <jamalfiqri@gmail.com>
2019-09-21 10:34:20 +0800

    Memperbaiki redirect setelah upload dan menambahkan preloader uploading files

--- application/views/site/uploadfiles.php
+++ application/views/site/uploadfiles.php
@@ -1,20 +1,18 @@
-
-
 <h4 class="center-align">{{ kategori.nama_bagian }}</h4>
 <h3 class="center-align">Upload File Data Kategori {{ kategori.nama }}</h3>
 <h6 class="center-align">Provinsi {{ provinsi.nama }}</h6><br>
-<form action="<?= base_url()?>data/import" method="post">
+<form action="<?= base_url() ?>site/index/<?= $id_kategori ?>" method="post">
 
-<?php 
+    <?php
     $this->load->view('site/_formuploadfiles');
     $this->load->view('templates/footer');
-?>
+    ?>
 
-<script type="text/javascript">
-	var id_kategori = '<?= $id_kategori ?>';
-	main.loadKategori(id_kategori);
-    main.loadSumberData();
-    main.loadKabKota();
-    var lokasi = "<?= base_url()?>site/index/"+id_kategori;
-    main.lokasi = lokasi;
-</script>
+    <script type="text/javascript">
+        var id_kategori = '<?= $id_kategori ?>';
+        main.loadKategori(id_kategori);
+        main.loadSumberData();
+        main.loadKabKota();
+        var lokasi = "<?= base_url() ?>site/index/" + id_kategori;
+        main.lokasi = lokasi;
+    </script>
