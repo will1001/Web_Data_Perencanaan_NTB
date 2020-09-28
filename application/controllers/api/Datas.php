c5f475290f4f36eb2276c6aaad2f1bdbf137c769
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-28 13:59:31 +0800

    api datas change limit

--- application/controllers/api/Datas.php
+++ application/controllers/api/Datas.php
@@ -17,13 +17,16 @@ class Datas extends REST_Controller {
         $limit = $this->get('limit');
         if ($limit == '') {
             $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            // $this->db->from("data")->limit(10,0);
             $this->db->from("data")->limit(10,0);
             $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
             $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
             $jsonData = $this->db->get()->result();
         } else {
-            $jsonData = $this->db->get('data',10,$limit)->result();
+             $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+            $this->db->from("data")->limit(10,$limit);
+            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+            $jsonData = $this->db->get()->result();
         }
 
         $this->response($jsonData, 200);
