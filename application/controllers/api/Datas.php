60d6a06aef580c147dd01293de3eba867aefa2e7
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-29 19:32:45 +0800

    add semester parameter in filter data (test logic 3)

--- application/controllers/api/Datas.php
+++ application/controllers/api/Datas.php
@@ -44,8 +44,8 @@ class Datas extends REST_Controller {
             $this->db->where('id_sumber_data', $sumber_data);
             $jsonData = $this->db->get()->result();
         }else if($semester != '') {
-            $this->db->select("MONTH(`tahun`),data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
+            $this->db->select("MONTH(`tahun`)");
+            $this->db->from("data");
             $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
             $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
             $this->db->where('id_kategori', $id_kategori);
