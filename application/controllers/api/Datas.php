de743bca417f4fc2c5d049a13ec31b0dbd5684fa
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-29 20:06:58 +0800

    add semester parameter in filter data (test logic 9)

--- application/controllers/api/Datas.php
+++ application/controllers/api/Datas.php
@@ -21,119 +21,153 @@ class Datas extends REST_Controller {
         $semester = $this->get('semester');
         $cari = $this->get('cari');
         $get_group_parameter = $this->get('get_group_parameter');
-        if ($limit == '' && $id_kategori == '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        if($limit != ''){
+            $this->db->from("data")->limit(10,$limit);
+        }else{
             $this->db->from("data")->limit(10,0);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $jsonData = $this->db->get()->result();
-        } else if($tahun != '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            $this->db->where('YEAR(`tahun`)', $tahun);
-            $jsonData = $this->db->get()->result();
-        }else if($sumber_data != '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            $this->db->where('id_sumber_data', $sumber_data);
-            $jsonData = $this->db->get()->result();
-        }else if($semester != '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            if($semester == '1'){
-                $this->db->where('MONTH(`tahun`) <', 7);
-            }if($semester == '2'){
-                $this->db->where('MONTH(`tahun`) >', 6);
-            }
-            $jsonData = $this->db->get()->result();
         }
-        else if($tahun != '' && $sumber_data != '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
+        $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        $this->db->where('id_kategori', $id_kategori);
+        
+        if($tahun != ''){
             $this->db->where('YEAR(`tahun`)', $tahun);
+        }else{
+            return '';
+        }
+        
+        if($sumber_data != ''){
             $this->db->where('id_sumber_data', $sumber_data);
-            $jsonData = $this->db->get()->result();
-        }else if($tahun != '' && $semester != '') {
-            $this->db->select("MONTH(`tahun`),data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            $this->db->where('YEAR(`tahun`)', $tahun);
-             if($semester == '1'){
-                $this->db->where('MONTH(`tahun`) <', 7);
-            }if($semester == '2'){
-                $this->db->where('MONTH(`tahun`) ', 3);
-            }
-            $jsonData = $this->db->get()->result();
-        }else if($semester != '' && $sumber_data != '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-             if($semester == '1'){
-                $this->db->where('MONTH(`tahun`) <', 7);
+        }else{
+            return '';
+        }
+       
+        if($semester != ''){
+            if($semester == '1'){
+             $this->db->where('MONTH(`tahun`) <', 7);
             }if($semester == '2'){
                 $this->db->where('MONTH(`tahun`) >', 6);
             }
-            $this->db->where('id_sumber_data', $sumber_data);
-            $jsonData = $this->db->get()->result();
-        }else if($tahun != '' && $sumber_data != '' && $semester == '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            $this->db->where('YEAR(`tahun`)', $tahun);
-            $this->db->where('id_sumber_data', $sumber_data);
-             if($semester == '1'){
-                $this->db->where('MONTH(`tahun`) <', 7);
-            }if($semester == '2'){
-                $this->db->where('MONTH(`tahun`) >', 6);
-            }
-            $jsonData = $this->db->get()->result();
-        }else if($cari != '') {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            $this->db->like('nama_data', $cari);
-            $jsonData = $this->db->get()->result();
-        }else if($get_group_parameter == 'tahun') {
-            $this->db->select("YEAR(`tahun`)");
-            $this->db->from("data");
-            $this->db->where('id_kategori', $id_kategori);
-            $this->db->group_by("tahun");
-            $jsonData = $this->db->get()->result();
-        }else if($get_group_parameter == 'sumber_data') {
-            $this->db->select("sumber_data.id,nama_sumber");
-            $this->db->from("data");
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            $this->db->group_by("nama_sumber");
-            $jsonData = $this->db->get()->result();
-        }else {
-            // $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            // $this->db->from("data")->limit(10,$limit);
-            // $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            // $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            // $this->db->where('id_kategori', $id_kategori);
-            $jsonData = [];
-        } 
+        }else{
+            return '';
+        }
+        
+        
+        $jsonData = $this->db->get()->result();
+        // if ($limit == '' && $id_kategori == '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,0);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $jsonData = $this->db->get()->result();
+        // } else if($tahun != '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->where('YEAR(`tahun`)', $tahun);
+        //     $jsonData = $this->db->get()->result();
+        // }else if($sumber_data != '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->where('id_sumber_data', $sumber_data);
+        //     $jsonData = $this->db->get()->result();
+        // }else if($semester != '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     if($semester == '1'){
+        //         $this->db->where('MONTH(`tahun`) <', 7);
+        //     }if($semester == '2'){
+        //         $this->db->where('MONTH(`tahun`) >', 6);
+        //     }
+        //     $jsonData = $this->db->get()->result();
+        // }
+        // else if($tahun != '' && $sumber_data != '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->where('YEAR(`tahun`)', $tahun);
+        //     $this->db->where('id_sumber_data', $sumber_data);
+        //     $jsonData = $this->db->get()->result();
+        // }else if($tahun != '' && $semester != '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->where('YEAR(`tahun`)', $tahun);
+        //      if($semester == '1'){
+        //         $this->db->where('MONTH(`tahun`) <', 7);
+        //     }if($semester == '2'){
+        //         $this->db->where('MONTH(`tahun`) >', 6);
+        //     }
+        //     $jsonData = $this->db->get()->result();
+        // }else if($semester != '' && $sumber_data != '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //      if($semester == '1'){
+        //         $this->db->where('MONTH(`tahun`) <', 7);
+        //     }if($semester == '2'){
+        //         $this->db->where('MONTH(`tahun`) >', 6);
+        //     }
+        //     $this->db->where('id_sumber_data', $sumber_data);
+        //     $jsonData = $this->db->get()->result();
+        // }else if($tahun != '' && $sumber_data != '' && $semester == '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->where('YEAR(`tahun`)', $tahun);
+        //     $this->db->where('id_sumber_data', $sumber_data);
+        //      if($semester == '1'){
+        //         $this->db->where('MONTH(`tahun`) <', 7);
+        //     }if($semester == '2'){
+        //         $this->db->where('MONTH(`tahun`) >', 6);
+        //     }
+        //     $jsonData = $this->db->get()->result();
+        // }else if($cari != '') {
+        //     $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     $this->db->from("data")->limit(10,$limit);
+        //     $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->like('nama_data', $cari);
+        //     $jsonData = $this->db->get()->result();
+        // }else if($get_group_parameter == 'tahun') {
+        //     $this->db->select("YEAR(`tahun`)");
+        //     $this->db->from("data");
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->group_by("tahun");
+        //     $jsonData = $this->db->get()->result();
+        // }else if($get_group_parameter == 'sumber_data') {
+        //     $this->db->select("sumber_data.id,nama_sumber");
+        //     $this->db->from("data");
+        //     $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     $this->db->where('id_kategori', $id_kategori);
+        //     $this->db->group_by("nama_sumber");
+        //     $jsonData = $this->db->get()->result();
+        // }else {
+        //     // $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+        //     // $this->db->from("data")->limit(10,$limit);
+        //     // $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+        //     // $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+        //     // $this->db->where('id_kategori', $id_kategori);
+        //     $jsonData = [];
+        // } 
 
         $this->response($jsonData, 200);
     }
