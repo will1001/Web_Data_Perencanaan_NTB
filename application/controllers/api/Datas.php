7b5048d353f5d860a9932c64e674329ed13eedce
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-29 19:48:30 +0800

    add semester parameter in filter data (test logic 5)

--- application/controllers/api/Datas.php
+++ application/controllers/api/Datas.php
@@ -127,12 +127,12 @@ class Datas extends REST_Controller {
             $this->db->group_by("nama_sumber");
             $jsonData = $this->db->get()->result();
         }else {
-            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
-            $this->db->from("data")->limit(10,$limit);
-            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
-            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
-            $this->db->where('id_kategori', $id_kategori);
-            $jsonData = $this->db->get()->result();
+            // $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+            // $this->db->from("data")->limit(10,$limit);
+            // $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+            // $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+            // $this->db->where('id_kategori', $id_kategori);
+            $jsonData = [];
         } 
 
         $this->response($jsonData, 200);
