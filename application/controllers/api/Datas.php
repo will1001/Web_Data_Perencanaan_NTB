1e35c1d78a95031c22ac9608f9f79f921083d4b1
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-29 11:25:42 +0800

    api datas add parameter id_kategori

--- application/controllers/api/Datas.php
+++ application/controllers/api/Datas.php
@@ -15,17 +15,19 @@ class Datas extends REST_Controller {
     //Menampilkan data label
     function index_get() {
         $limit = $this->get('limit');
-        if ($limit == '') {
+        $id_kategori = $this->get('id_kategori');
+        if ($limit == '' && $id_kategori == '') {
             $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
             $this->db->from("data")->limit(10,0);
             $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
             $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
             $jsonData = $this->db->get()->result();
         } else {
-             $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
             $this->db->from("data")->limit(10,$limit);
             $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
             $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+            $this->db->where('id_kategori', $id_kategori);
             $jsonData = $this->db->get()->result();
         }
 
