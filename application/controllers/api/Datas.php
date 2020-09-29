6319f8227a967fc08788110db011f9b4d0879045
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-29 12:02:15 +0800

    api datas add parameter for filter feature

--- application/controllers/api/Datas.php
+++ application/controllers/api/Datas.php
@@ -16,20 +16,47 @@ class Datas extends REST_Controller {
     function index_get() {
         $limit = $this->get('limit');
         $id_kategori = $this->get('id_kategori');
+        $tahun = $this->get('tahun');
+        $sumber_data = $this->get('sumber_data');
         if ($limit == '' && $id_kategori == '') {
             $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
             $this->db->from("data")->limit(10,0);
             $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
             $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
             $jsonData = $this->db->get()->result();
-        } else {
+        } else if($tahun == '') {
+            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+            $this->db->from("data")->limit(10,$limit);
+            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+            $this->db->where('id_kategori', $id_kategori);
+            $this->db->where('tahun', $tahun);
+            $jsonData = $this->db->get()->result();
+        }else if($sumber_data == '') {
+            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+            $this->db->from("data")->limit(10,$limit);
+            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+            $this->db->where('id_kategori', $id_kategori);
+            $this->db->where('id_sumber_data', $sumber_data);
+            $jsonData = $this->db->get()->result();
+        }else if($tahun == '' && $sumber_data == '') {
+            $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
+            $this->db->from("data")->limit(10,$limit);
+            $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
+            $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
+            $this->db->where('id_kategori', $id_kategori);
+            $this->db->where('tahun', $tahun);
+            $this->db->where('id_sumber_data', $sumber_data);
+            $jsonData = $this->db->get()->result();
+        }else {
             $this->db->select("data.id,nama_data,nilai,satuan,tahun,nama_sumber,nama");
             $this->db->from("data")->limit(10,$limit);
             $this->db->join('kab_kota', 'kab_kota.id = data.id_kab_kota','left');
             $this->db->join('sumber_data', 'sumber_data.id = data.id_sumber_data','left');
             $this->db->where('id_kategori', $id_kategori);
             $jsonData = $this->db->get()->result();
-        }
+        } 
 
         $this->response($jsonData, 200);
     }
