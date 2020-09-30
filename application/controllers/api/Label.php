67563dd64dbd9c7607ae484ae419ca3ac7c5e4a3
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-09-30 20:59:16 +0800

    test search logic 11

--- application/controllers/api/Label.php
+++ application/controllers/api/Label.php
@@ -20,13 +20,13 @@ class Label extends REST_Controller {
             $this->db->select("*");
             $this->db->from("label")->limit(10,$limit);
             $this->db->join('keterangan', 'keterangan.id_label = label.id','left');
-            $this->db->group_by('nama');
+            $this->db->group_by('id_label');
             $jsonData = $this->db->get()->result();
       }else if($limit != '' && $cari != '') {
             $this->db->select("*");
             $this->db->from("label")->limit(10,$limit);
             $this->db->join('keterangan', 'keterangan.id_label = label.id','left');
-            $this->db->group_by('nama');
+            $this->db->group_by('id_label');
             $this->db->like('nama', $cari);
             $jsonData = $this->db->get()->result();
         }else{
